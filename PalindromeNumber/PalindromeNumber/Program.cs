9ddd10ab582c5c9fed76a66a33af237b172dc53f
Rhea-Gomez <rhea.gomez@aurionpro.com>
2025-07-11 01:30:23 +0530

    Assignment on Palindrome Number

---
+++ PalindromeNumber/PalindromeNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace PalindromeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, remainder, reverse = 0, temp;
+            Console.WriteLine("Enter a number: ");
+            num = int.Parse(Console.ReadLine());
+
+            temp = num;
+            while(temp > 0)
+            {
+                remainder = temp % 10;
+                reverse = 10 * reverse + remainder;
+                temp = temp / 10;
+            }
+
+            if(reverse == num)
+            {
+                Console.WriteLine(num + " is a Palindrome number");
+            } else
+            {
+                Console.WriteLine(num + " is not a Palindrome number");
+            }
+
+        }
+    }
+}
